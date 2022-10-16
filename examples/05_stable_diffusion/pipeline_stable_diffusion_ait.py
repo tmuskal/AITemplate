1a18ad0d1d852bf438b9618157ca06da09c5750d
Tal Muskal <tmuskal@gmail.com>
2022-10-16 16:36:04 +0300

    adding inversion

--- examples/05_stable_diffusion/pipeline_stable_diffusion_ait.py
+++ examples/05_stable_diffusion/pipeline_stable_diffusion_ait.py
@@ -416,19 +416,19 @@ class StableDiffusionAITPipeline(StableDiffusionPipeline):
         image = image.cpu().permute(0, 2, 3, 1).numpy()
 
         # run safety checker
-        safety_cheker_input = self.feature_extractor(
-            self.numpy_to_pil(image), return_tensors="pt"
-        ).to(self.device)
-        image, has_nsfw_concept = self.safety_checker(
-            images=image, clip_input=safety_cheker_input.pixel_values
-        )
+        # safety_cheker_input = self.feature_extractor(
+        #     self.numpy_to_pil(image), return_tensors="pt"
+        # ).to(self.device)
+        # image, has_nsfw_concept = self.safety_checker(
+        #     images=image, clip_input=safety_cheker_input.pixel_values
+        # )
 
         if output_type == "pil":
             image = self.numpy_to_pil(image)
 
         if not return_dict:
-            return (image, has_nsfw_concept)
+            return (image, False)
 
         return StableDiffusionPipelineOutput(
-            images=image, nsfw_content_detected=has_nsfw_concept
+            images=image, nsfw_content_detected=False
         )
