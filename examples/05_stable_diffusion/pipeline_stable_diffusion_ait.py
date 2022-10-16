b12a9a9a18f03697d6d3b61bf2e21c8796415260
Tal Muskal <tmuskal@gmail.com>
2022-10-16 14:38:40 +0300

    adding inversion

--- examples/05_stable_diffusion/pipeline_stable_diffusion_ait.py
+++ examples/05_stable_diffusion/pipeline_stable_diffusion_ait.py
@@ -47,8 +47,8 @@ def embed_inversion(
         progressive_words = False
 ):
     print(embedded_text)
-    tokenized_text = tokenized_text
-    embedded_text = embedded_text
+    tokenized_text = tokenized_text.data
+    embedded_text = embedded_text.data
     print(embedded_text.shape)
     print(tokenized_text)
     (prompts,b, n) = tokenized_text.shape
