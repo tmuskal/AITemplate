af79103122d7ab1f09eab1e4cd735d97953adde1
Tal Muskal <tmuskal@gmail.com>
2022-10-21 23:23:37 +0300

    adding docker builder

--- examples/05_stable_diffusion/server.py
+++ examples/05_stable_diffusion/server.py
@@ -49,4 +49,4 @@ def render():
             sem.release()
 
 if __name__ == '__main__':
-    create_app().run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
